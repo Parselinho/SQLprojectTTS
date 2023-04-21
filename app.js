13b440d0d0b8f79401f49710ef1ff913197deb8a
Gal Parselani <Parselinho@gmail.com>
2023-04-21 12:39:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/books');
 
 var app = express();
 
@@ -33,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/books', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
