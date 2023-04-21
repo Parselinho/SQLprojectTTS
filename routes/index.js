b029249ed77b093ae4fcec56fe20114194eb20a1
Gal Parselani <Parselinho@gmail.com>
2023-04-21 12:24:58 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,14 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
 const { Book } = require('../models');
 
-/* GET home page. */
-router.get('/', async function(req, res, next) {
-  try {
-    const books = await Book.findAll();
-    res.json(books.map(book => book.toJSON())); // Send the books as JSON response
-  } catch (error) {
-    next(error); // Pass the error to the next middleware (error handler)
-  }
+router.get('/books', async (req, res, next) => {
+    try {
+        const books = await Book.findAll();
+        res.render('index', { books });
+    } catch (error) {
+        next(error);
+    }
 });
 
 module.exports = router;
